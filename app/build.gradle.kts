ddc9fa301d6585bbd8921b5e95ac7b9771462fd2
you <you@example.com>
2026-09-12 04:12:49 +0900

    Bump release version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.messpy.aimodelbridge"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
